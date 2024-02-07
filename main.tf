e5f36857dfa7fdfccfe229a3042a2dc79e9ef28b
dC <131455625+dcolanderjr@users.noreply.github.com>
2024-02-07 11:16:47 -0500

    Static Resume Hosting

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
+# Provider for Terraform, AWS, and working region
 provider "aws" {
     region = "us-east-1"
 }
 
+# Create bucket, and add desired tags(optional). For hosting a static website on S3, you must use the format xxx.com, or whatever
+# you want to call your website. I am using kloudkamp.com for this example, which I registered in Route53.
 resource "aws_s3_bucket" "dcolanderjr_resume" {
     bucket = "kloudkamp.com"
     
     tags = {
         Name = "dcolanderjr_resume"
         Environment = "Prod"
-        Team = "DevOps"
         Application = "Resume Website"
         Terraform = "True"
         Owner = "dcolanderjr@kloudkamp.com"
     }
 }
 
+# This enables versioning on the bucket, so that you can keep track of changes to your website as it changes.
 resource "aws_s3_bucket_versioning" "dcolanderjr_resume" {
     bucket = "kloudkamp.com"
     versioning_configuration {
@@ -22,6 +25,7 @@ resource "aws_s3_bucket_versioning" "dcolanderjr_resume" {
     }
 }
 
+# This enables the bucket to be publically accessible, so that your website can be viewed from the internet.
 resource "aws_s3_bucket_public_access_block" "dcolanderjr_resume" {
     bucket = "kloudkamp.com"
 
@@ -29,9 +33,31 @@ resource "aws_s3_bucket_public_access_block" "dcolanderjr_resume" {
     restrict_public_buckets = false
 }
 
+# This enables the "Static Website Hosting" option that is under the permissions in the S3 Console. The index.html
+# refers to your index.html file in the root directory of your website.
 resource "aws_s3_bucket_website_configuration" "dcolanderjr_resume" {
     bucket = "kloudkamp.com"
     index_document {
         suffix = "index.html"
     }
+}
+
+# This creates the S3 bucket policy that allows your website to be publicly viewable by others on the internet.
+  
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = "kloudkamp.com"
+  policy = jsonencode(
+    {
+      "Version" : "2024-02-07",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : "arn:aws:s3:::kloudkamp.com/*"
+        }
+      ]
+    }
+  )
 }
